931fe318a1e2dbc0df16058b4efe39c56be64b75
Adam L <alane019@gmail.com>
2021-01-20 01:37:56 -0500

    update seed.sql after mysql workbench overwrite

--- seed.sql
+++ seed.sql
@@ -1,15 +1,8 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ('Ronald','McDouglas','1','4'),
-('Maria','Doe','2','1'), ('Chuck','Logan','3','1'),
-('Rebecca','Smith','4','4');
-
+INSERT INTO department (name)
+VALUES ('admin'), ('finance'), ('engineering'), ('legal');
 
 INSERT INTO role (title, salary, department_id)
-VALUES ('manager','90000', '1'),
- ('sales assoc','75000', '2'), ('engineer','80000', '3'),
- ('CEO','200000', '1'), ('sales intern','25000', '2');
+VALUES ('manager', 90000, 1), ('sales assoc', 75000, 2), ('engineer', 80000, 3), ('CEO', 200000, 1), ('sales intern', 25000, 2);
 
- INSERT INTO department (name)
- VALUES ('admin'), ('finance'), ('engineering'), ('legal');
-
-
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ronald','McDouglas', 1, 4), ('Maria','Doe', 2, 1), ('Chuck','Logan', 3, 1), ('Rebecca','Smith', 4, 4);
